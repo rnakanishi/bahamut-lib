9d397a14273e65383fd53f185ae4d0b77ab980d1
Rafael Nakanishi <rafael.umino.nakanishi@gmail.com>
2019-11-20 08:44:15 +0900

    Added python script for enright statistics

--- moogle/enright_statistics.py
+++ moogle/enright_statistics.py
@@ -17,13 +17,20 @@ f, axes = plt.subplots(1, 2, figsize=(12, 6))
 
 total_df = pandas.DataFrame()
 cellAdv_df = pandas.DataFrame()
+global_df = pandas.DataFrame()
 # Total times and plot
 for file in files:
     dataname = file[:-8]
     table = pandas.read_csv(file, delimiter=', ', engine="python")
 
-    time = table.loc[:, table.columns != 'total']
-    time = time.loc[:, time.columns != 'redistance']
+    time = table.loc[:, table.columns != 'extractSurface']
+    time = time.loc[:, time.columns != 'total']
+#     time = time.loc[:, time.columns != 'redistance']
+
+    total = table.loc[:, table.columns == 'total']
+    df = total.rename(columns={"total": dataname})
+    global_df = pandas.concat([global_df, df], axis=1, sort=False)
+
     total = time.sum(axis=1)
     df = pandas.DataFrame({dataname: total})
     total_df = pandas.concat([total_df, df], axis=1, sort=False)
@@ -33,12 +40,22 @@ for file in files:
     cellAdv_df = pandas.concat(
         [cellAdv_df, df], axis=1, sort=False)
 
-sea.lineplot(data=total_df, dashes=False, ax=axes[0])
-axes[0].set(ylabel="seconds", xlabel="timesteps")
-axes[0].set_title("Total time")
+print(global_df.head())
 
-sea.lineplot(data=cellAdv_df, dashes=False, ax=axes[1])
-axes[1].set(ylabel="seconds", xlabel="timesteps")
-axes[1].set_title("Cell advection")
+axisCount = 0
+
+sea.lineplot(data=global_df, dashes=False, ax=axes[axisCount])
+axes[axisCount].set(ylabel="seconds", xlabel="Frames")
+axes[axisCount].set_title("Total time")
+axisCount += 1
+
+# sea.lineplot(data=total_df, dashes=False, ax=axes[axisCount])
+# axes[axisCount].set(ylabel="seconds", xlabel="Frames")
+# axes[axisCount].set_title("Average time")
+# axisCount+=1;
+
+sea.lineplot(data=cellAdv_df, dashes=False, ax=axes[axisCount])
+axes[axisCount].set(ylabel="seconds", xlabel="timesteps")
+axes[axisCount].set_title("Cell advection average")
 
 plt.show()
